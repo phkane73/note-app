b6e6e1c96cce1d16171890d9e15062550ca83bc3
phkane73 <phkane732002@gmail.com>
2024-04-30 20:08:55 +0700

    update client

--- client/src/utils/constains.js
+++ client/src/utils/constains.js
@@ -1,2 +1,3 @@
-export const GRAPHQL_SERVER = "http://localhost:4000";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://localhost:4000/graphql";
+export const GRAPHQL_SERVER = "https://note-app-server-cq61.onrender.com";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
+  "wss://note-app-server-cq61.onrender.com/graphql";
